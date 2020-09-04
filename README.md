d319f66d7e2eb9d4dbb763d58ec8e2a6bbcf1ba1
Kevin Ziegler <ziegler.kevin@heb.com>
2020-09-04 11:24:17 -0500

    Add initial version of OPAV plugin

---
+++ README.md
@@ -0,0 +1,58 @@
+# OP AWS Vault
+Overpowered AWS Vault (with 1Password)!
+
+Do you use AWS?
+Do you use `aws-vault` to run commands?
+Do you use 1Password?
+
+Are you tired of having to leave your terminal, open 1Password, find your AWS account, copy the MFA, and then go back to your terminal and enter your token before it resets, LIKE A PLEB?  Then this is the plugin for you!  With OPAV, you can generate MFA tokens to be auto-magically inserted into your `aws-vault` commands with ease!  Amaze your friends!  Terrify your enemies with your lightning-fast aws commands!  Install OPAV today!
+
+## Requirements
+The following programs should be installed to use this plugin:
+* [aws-vault](https://github.com/99designs/aws-vault)
+* [1password](https://1password.com/)
+* [1password CLI](https://1password.com/downloads/command-line/)
+* [fzf](https://github.com/junegunn/fzf)
+* [jq](https://stedolan.github.io/jq/)
+
+On OSX, all requirements can be installed via Homebrew:
+``` shell
+brew install fzf jq
+brew cask install aws-vault 1password 1password-cli
+```
+
+## Installation
+### ZSH
+For ZSH, its configuration file, restart your shell to pick up the changes
+## Usage
+This plugin adds 3 aliases:
+* `opav` - Use this in place of aws-vault for an over-powered experience
+* `avp` - Use the power of FZF to select an aws profile!
+* `opav-setup` - Configure opav
+
+### Setup
+1. Be sure you have 1Password setup on your computer with your 1Password account
+2. [Configure 1Password as your AWS MFA provider](https://support.1password.com/one-time-passwords/)
+3. Run `opav-setup` to select the 1Password item containing your AWS account credentials
+``` shellsession
+opav-setup
+```
+
+### Running aws-vault commands with opav
+`opav` aims to act as a simple wrapper around the aws-vault binary.  Where you would use `aws-vault`, simply substitute `opav`:
+
+``` shellsession
+aws-vault exec <aws profile> -- aws sts get-caller-identity
+opav exec <aws profile> -- aws sts get-caller-identity
+```
+
+When running a command that requires an MFA token, OPAV will launch a 1Password session in your terminal.  You'll be prompted for your 1Password master password, and then `opav` will fetch an MFA token to use for AWS authentication.  Finally, no more jumping between windows for MFA codes!
+
+### Selecting profiles for use with aws-vault
+Remembering things is hard and lame, especially when it's that random aws profile alias you almost never need and have to look up every time you use it.  Why bother memorizing things when you can use your awesome FZF powers instead?
+
+The OPAV plugin also installs an alias (`avp`) for selecting a profile to use when running `aws-vault`/`opav` commands.  Simply run the alias as a subshell where you want to insert the profile in your command:
+``` shellsession
+aws-vault exec $(avp) -- <some command>
+opav exec $(avp) -- <some command>
+```
---
+++ opav.plugin.zsh
@@ -0,0 +1,91 @@
+#!/bin/bash
+export OPAV_CONFIG=${OPAV_CONFIG:-"$HOME/.config/opav/config.json"};
+
+# Preview helper for 1Password items in fzf
+function opav-preview-op-item() {
+    local op_item;
+    local op_vault;
+    local op_item_uuid=$2;
+
+    op_item=$(op get item "$op_item_uuid");
+    op_vault=$(jq -r '.vaultUuid' <<< "$op_item" | op get vault - | jq '.name')
+
+    echo "[Item] %F{cyan}$1"
+    echo "[Vault] %F${op_vault:-UNKNOWN}"
+}
+
+# Select the 1Password item to use for generating MFA codes
+function opav-select-op-aws-item() {
+    mkdir -p "$(dirname "$OPAV_CONFIG")";
+
+    # Steps:
+    # 1. (jq)          Extract Title/UUID from 1Password as a CSV
+    # 2. (sed)         Strip out quotes (for display purposes)
+    # 3. (echo && cat) Prepend a header
+    # 4. (column)      Tabulate items for display in FZF
+    # 5. (fzf)         Select a 1Password item
+    # 6. (awk)         Extract the UUID of the selected item
+    # 7. (jq)          write the UUID to the opav config
+    op list items \
+        | jq -r '.[] | [.overview.title, .uuid] | @csv' \
+        | sed 's/"//g' \
+        | (echo "TITLE,UUID" && cat) \
+        | column -t -s, \
+        | fzf \
+            --header "Select the 1Password item for your AWS account" \
+            --header-lines 1 \
+            -1 \
+            --preview-window=down:2 \
+            --preview='opav-preview-op-item {}' \
+        | awk '{print $NF}' \
+        | jq -R '{ "op_aws_item": . }' > "$OPAV_CONFIG";
+}
+
+# Ensure the user has a 1Password session in their current
+# shell.  Attempt to restart it if not.
+function opav-ensure-session() {
+    until op get account &>/dev/null; do
+        eval "$(op signin my)";
+    done
+}
+
+function opav-get-aws-item-uuid() {
+    jq -re '.op_aws_item' "$OPAV_CONFIG" 2>/dev/null;
+    return $?;
+}
+
+# Get the MFA token for a user to sign into AWS
+function opav-mfa-token() {
+    until op get totp "$(opav-get-aws-item-uuid)" 2>/dev/null; do
+        print "Could not get MFA token.  Running setup..." >&2
+        sleep 2
+        opav-select-op-aws-item;
+    done
+}
+
+# Run a command in aws-vault without worrying about MFA args
+function opav-helper() {
+    local mfa_token='';
+    local command=$1;
+    shift;
+
+    # Generate an MFA token and argument if the given command
+    # can accept it
+    [[ "$command" =~ ^(login|exec)$ ]] &&
+        opav-ensure-session &&
+        mfa_token=$(opav-mfa-token);
+
+    aws-vault "$command" ${mfa_token:+--mfa-token="$mfa_token"} "$@";
+}
+
+# Select a profile for use with aws-vault
+function aws-vault-profile-helper() {
+    aws-vault list \
+        | fzf --header-lines 2 --header "Select an AWS Profile" \
+        | cut -d' ' -f1
+}
+
+# Setup Aliases for the plugin
+alias opav-setup="opav-ensure-session && opav-select-op-aws-item"
+alias opav="opav-helper"
+alias avp="aws-vault-profile-helper"
