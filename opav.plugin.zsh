6d13d51c23af82a57f5e2304434686c795dabacd
Kevin Ziegler <ziegler.kevin@heb.com>
2020-09-08 13:09:26 -0500

    Clean up autoloading code for ZSH

--- opav.plugin.zsh
+++ opav.plugin.zsh
@@ -1,14 +1,8 @@
 SOURCE_PATH="${0:h}"
 
 fpath+="$SOURCE_PATH/funcs"
+autoload -Uz $SOURCE_PATH/funcs/*(.:t)
 
 source "$SOURCE_PATH/vars.sh"
 source "$SOURCE_PATH/aliases.sh"
 
-function opav-init() {
-    for func in "$SOURCE_PATH"/funcs/*; do
-        autoload -Uz $func
-    done
-}
-
-opav-init
