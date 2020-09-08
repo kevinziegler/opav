a0985da04197432497137407d7cd0beed3cf8f0b
Kevin Ziegler <ziegler.kevin@heb.com>
2020-09-08 12:45:39 -0500

    Attempt to see init operations in zprof

--- opav.plugin.zsh
+++ opav.plugin.zsh
@@ -2,9 +2,13 @@ SOURCE_PATH="${0:h}"
 
 fpath+="$SOURCE_PATH/funcs"
 
-for func in "$SOURCE_PATH"/funcs/*; do
-    autoload -Uz $func
-done
-
 source "$SOURCE_PATH/vars.sh"
 source "$SOURCE_PATH/aliases.sh"
+
+function opav-init() {
+    for func in "$SOURCE_PATH"/funcs/*; do
+        autoload -Uz $func
+    done
+}
+
+opav-init
